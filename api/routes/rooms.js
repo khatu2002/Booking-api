6f88d3e642f4418180f7c698df3f53eff05585a2
khatu2002 <20522096@gm.uit.edu.vn>
2023-12-15 01:09:44 +0700

    commit

---
+++ api/routes/rooms.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { updateRoomAvailability, createRoom, deleteRoom, updateRoom, getRoom, getRooms } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+const router = express.Router();
+
+//CREATE
+router.post("/:hotelid", createRoom);
+
+//UPDATE
+router.put("/:id", updateRoom)
+router.put("/availability/:id", updateRoomAvailability)
+
+//DELETE
+router.delete("/:id/:hotelid", deleteRoom)
+//GET
+router.get("/:id", getRoom)
+//GET ALL
+router.get("/", getRooms)
+
+export default router
